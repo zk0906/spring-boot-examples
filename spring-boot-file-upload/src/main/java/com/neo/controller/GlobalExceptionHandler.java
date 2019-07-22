61da46b445d7657f5d2897c3098b40a99154ab83
zkpc <14496505@qq.com>
2019-07-22 22:27:48 +0800

    1.多文件上传 2.上传大小限制

--- spring-boot-file-upload/src/main/java/com/neo/controller/GlobalExceptionHandler.java
+++ spring-boot-file-upload/src/main/java/com/neo/controller/GlobalExceptionHandler.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+/**
+ * 设置一个@ControllerAdvice用来监控Multipart上传的文件大小是否受限，当出现此异常时在前端页面给出提示。利用@ControllerAdvice可以做很多东西，比如全局的统一异常处理等，感兴趣的同学可以下来了解。
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
--- spring-boot-file-upload/src/main/java/com/neo/controller/UploadController.java
+++ spring-boot-file-upload/src/main/java/com/neo/controller/UploadController.java
@@ -1,30 +1,43 @@
 package com.neo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @Controller
 public class UploadController {
     //Save the uploaded file to this folder
-    private static String UPLOADED_FOLDER = "E://temp//";
+    private static String UPLOADED_FOLDER = "D:\\studyroom3\\springboot\\tmp\\";
 
     @GetMapping("/")
     public String index() {
         return "upload";
     }
 
+    @GetMapping("/upload")
+    public String upload() {
+        return "upload";
+    }
+
+    @GetMapping("/multiUpload")
+    public String multiUpload() {
+        return "multiUpload";
+    }
+
     @PostMapping("/upload") // //new annotation since 4.3
-    public String singleFileUpload(@RequestParam("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
+    public String singleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
         if (file.isEmpty()) {
             redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
             return "redirect:uploadStatus";
@@ -32,15 +45,44 @@ public class UploadController {
 
         try {
             // Get the file and save it somewhere
+            //写入方法一
             byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
             Files.write(path, bytes);
 
+            //写入方法二
+//            File dest = new File(UPLOADED_FOLDER + file.getOriginalFilename());
+//            file.transferTo(dest);
             redirectAttributes.addFlashAttribute("message",
                     "You successfully uploaded '" + file.getOriginalFilename() + "'");
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+    @PostMapping("/multiUpload") // //new annotation since 4.3
+    public String multiFileUpload(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("file");
+        if (CollectionUtils.isEmpty(files)) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+        for(MultipartFile file :files){
+            try {
+                // Get the file and save it somewhere
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+                Files.write(path, bytes);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
         }
 
         return "redirect:/uploadStatus";
